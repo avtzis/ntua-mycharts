4f5cca472dd7078c0c2b7cdaedee967920d85d71
avtzis <avtzialtan@hotmail.com>
2023-03-28 13:30:01 +0300

    added themes and dark mode

--- ui/src/routes/Layout.js
+++ ui/src/routes/Layout.js
@@ -1,25 +1,34 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-import { AppBar, Container, Toolbar } from '@mui/material'
+import { AppBar, Container, CssBaseline, Switch, ThemeProvider, Toolbar } from '@mui/material'
 import Copyright from '../components/Copyright';
+import darkTheme from '../utilities/darkTheme';
+import mainTheme from '../utilities/mainTheme';
 
 const Layout = () => {
+  const [darkMode, setDarkMode] = React.useState(false);
+
+  const handleChange = (event) => {
+    setDarkMode(event.target.checked);
+  };
+
   return (
-    <React.Fragment>
-      {<AppBar position='fixed' sx={{mb: 0}}>
+    <ThemeProvider theme={darkMode ? darkTheme : mainTheme}>
+      <CssBaseline />
+      <AppBar position='fixed' sx={{mb: 0}}>
         <Container maxWidth='xl'>
           <Toolbar disableGutters>
-
+            <Switch checked={darkMode} onChange={handleChange} />
           </Toolbar>
         </Container>
-      </AppBar>}
-      <main style={{position: 'relative', paddingTop: 60}}>
+      </AppBar>
+      <main style={{position: 'relative', paddingTop: 75}}>
         <Outlet />
       </main>
       <footer>
         <Copyright />
       </footer>
-    </React.Fragment>
+    </ThemeProvider>
   )
 }
 
---
+++ ui/src/utilities/mainTheme.js
@@ -0,0 +1,8 @@
+import { createTheme } from "@mui/material";
+
+const mainTheme = createTheme({
+  //  https://mui.com/material-ui/customization/theming/#theme-configuration-variables
+  //  https://mui.com/material-ui/customization/default-theme/
+});
+
+export default mainTheme;
