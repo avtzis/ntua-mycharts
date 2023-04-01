6a8f427ec1fa9a5de8c75f987157dbc06b7b6ff8
ntua-el19241 <el19241@mail.ntua.gr>
2023-04-01 17:12:57 +0300

    added search field

--- ui/src/routes/Layout.js
+++ ui/src/routes/Layout.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 
 // Material UI Components
-import { AppBar, Container, CssBaseline, Switch, ThemeProvider, Toolbar, useMediaQuery, Button, Box } from '@mui/material'
+import { AppBar, CssBaseline, Switch, ThemeProvider, Toolbar, useMediaQuery, Button, Box, IconButton, Typography, InputBase } from '@mui/material'
 
 // Custom Components
 import Copyright from '../components/Copyright';
@@ -14,17 +14,28 @@ import mainTheme from '../utilities/mainTheme';
 // Font
 import 'fontsource-roboto';
 
+// Icons
+import MenuIcon from '@mui/icons-material/Menu';
+import AutoGraphIcon from '@mui/icons-material/AutoGraph';
+import SearchIcon from '@mui/icons-material/Search';
+
 // Utilities
 import { useCookies } from 'react-cookie';
 import axios from 'axios';
 import api from '../utilities/api';
+import { alpha } from '@mui/material/styles'
 
 const Layout = () => {
   const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
   const [cookies, setCookie] = useCookies(['dark']); 
   const [darkMode, setDarkMode] = React.useState(cookies['dark'] === undefined ? (prefersDarkMode === 'false' ? false : true) : (cookies['dark'] === 'false' ? false : true));
+  const [theme, setTheme] = React.useState(mainTheme);
 
   axios.defaults.withCredentials = true;
+
+  React.useEffect(() => {
+    setTheme(darkMode ? darkTheme : mainTheme);
+  }, [darkMode]);
 
   const handleChange = (event) => {
     setDarkMode(event.target.checked);
@@ -48,21 +59,30 @@ const Layout = () => {
   }
 
   return (
-    <ThemeProvider theme={darkMode ? darkTheme : mainTheme}>
+    <ThemeProvider theme={theme}>
       <CssBaseline />
-      <AppBar position='fixed' sx={{mb: 0}}>
-        <Container maxWidth='xl'>
-          <Toolbar disableGutters>
-            <Box sx={{display: 'flex'}}>
-              <Switch checked={darkMode} onChange={handleChange} />
-              <Button onClick={handleVerify} variant='contained' color='secondary'>Verify Login</Button>
-              <Button onClick={handleLogout} variant='contained' color='secondary'>Logout</Button>
-              <Button href='/create' variant='contained' color='secondary'>Create</Button>
-            </Box>
-          </Toolbar>
-        </Container>
+      <AppBar position='fixed' sx={{py: 1}}>
+        <Toolbar>
+          <IconButton aria-label='hamburger' color='inherit' size='large' edge='start'>
+            <MenuIcon />
+          </IconButton>
+          <AutoGraphIcon fontSize='large' sx={{mr: 1}}/>
+          <Typography variant='h6' href='/' component='a' noWrap sx={{fontFamily: 'monospace', fontWeight: 700, color: 'inherit', textDecoration: 'none', letterSpacing: '.1rem'}}>
+            myCharts
+          </Typography>
+          <Box sx={{display: 'flex'}}>
+            <Switch checked={darkMode} onChange={handleChange} />
+            <Button onClick={handleVerify} color='inherit'>Verify Login</Button>
+            <Button onClick={handleLogout} color='inherit'>Logout</Button>
+            <Button href='/create' color='inherit'>Create</Button>
+          </Box>
+          <Box sx={{backgroundColor: alpha(theme.palette.common.white, 0.15), '&:hover': {backgroundColor: alpha(theme.palette.common.white, 0.25)}, p: 1, display: 'flex', justifyContent: 'center', alignItems: 'center', width: 'auto'}}>
+            <SearchIcon />
+            <InputBase placeholder='Search...' sx={{color: 'inherit', pl: 1, transition: theme.transitions.create('width'), width: 'auto'}} />
+          </Box>
+        </Toolbar>
       </AppBar>
-      <main style={{position: 'relative', paddingTop: 75}}>
+      <main style={{position: 'relative', paddingTop: 100}}>
         <Outlet />
       </main>
       <footer>
