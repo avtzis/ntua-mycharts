f84845939b5273338e73455e8d1136db5f40fdf1
ntua-el19241 <el19241@mail.ntua.gr>
2023-04-05 16:43:32 +0300

    added drawer

--- ui/src/routes/Layout.js
+++ ui/src/routes/Layout.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 
 // Material UI Components
-import { AppBar, CssBaseline, Switch, ThemeProvider, Toolbar, useMediaQuery, Button, Box, IconButton, Typography, InputBase } from '@mui/material'
+import { AppBar, CssBaseline, Switch, ThemeProvider, Toolbar, useMediaQuery, Button, Box, IconButton, Typography, InputBase, Drawer, Divider, List, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material'
 
 // Custom Components
 import Copyright from '../components/Copyright';
@@ -18,6 +18,7 @@ import 'fontsource-roboto';
 import MenuIcon from '@mui/icons-material/Menu';
 import AutoGraphIcon from '@mui/icons-material/AutoGraph';
 import SearchIcon from '@mui/icons-material/Search';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 
 // Utilities
 import { useCookies } from 'react-cookie';
@@ -30,8 +31,23 @@ const Layout = () => {
   const [cookies, setCookie] = useCookies(['dark']); 
   const [darkMode, setDarkMode] = React.useState(cookies['dark'] === undefined ? (prefersDarkMode === 'false' ? false : true) : (cookies['dark'] === 'false' ? false : true));
   const [theme, setTheme] = React.useState(mainTheme);
+  const [open, setOpen] = React.useState(false);
 
   axios.defaults.withCredentials = true;
+
+  const transition = action => {
+    return theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: action === 'enter' ? theme.transitions.duration.enteringScreen : theme.transitions.duration.leavingScreen
+    })
+  };
+
+  const transitionMargin = action => {
+    return theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: action === 'enter' ? theme.transitions.duration.enteringScreen : theme.transitions.duration.leavingScreen
+    })
+  };
 
   React.useEffect(() => {
     setTheme(darkMode ? darkTheme : mainTheme);
@@ -61,33 +77,62 @@ const Layout = () => {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <AppBar position='fixed' sx={{py: 1}}>
-        <Toolbar>
-          <IconButton aria-label='hamburger' color='inherit' size='large' edge='start'>
-            <MenuIcon />
-          </IconButton>
-          <AutoGraphIcon fontSize='large' sx={{mr: 1}}/>
-          <Typography variant='h6' href='/' component='a' noWrap sx={{fontFamily: 'monospace', fontWeight: 700, color: 'inherit', textDecoration: 'none', letterSpacing: '.1rem'}}>
-            myCharts
-          </Typography>
-          <Box sx={{display: 'flex'}}>
-            <Switch checked={darkMode} onChange={handleChange} />
-            <Button onClick={handleVerify} color='inherit'>Verify Login</Button>
-            <Button onClick={handleLogout} color='inherit'>Logout</Button>
-            <Button href='/create' color='inherit'>Create</Button>
-          </Box>
-          <Box sx={{backgroundColor: alpha(theme.palette.common.white, 0.15), '&:hover': {backgroundColor: alpha(theme.palette.common.white, 0.25)}, p: 1, display: 'flex', justifyContent: 'center', alignItems: 'center', width: 'auto'}}>
-            <SearchIcon />
-            <InputBase placeholder='Search...' sx={{color: 'inherit', pl: 1, transition: theme.transitions.create('width'), width: 'auto'}} />
-          </Box>
-        </Toolbar>
-      </AppBar>
-      <main style={{position: 'relative', paddingTop: 100}}>
-        <Outlet />
-      </main>
-      <footer>
-        <Copyright />
-      </footer>
+      <Box sx={{display: 'flex'}}>
+        <AppBar position='fixed' sx={{zIndex: theme.zIndex.drawer + 1, py: 1, transition: transitionMargin('leave'), ...(open && {ml: 240, width: `calc(100% - ${240}px)`, transition: transitionMargin('enter')})}}>
+          <Toolbar sx={{pr: '24px'}}>
+            <IconButton aria-label='hamburger' color='inherit' size='large' edge='start' sx={{mr: '30px'}} onClick={() => setOpen(!open)}>
+              {open ? <ChevronLeftIcon /> : <MenuIcon />}
+            </IconButton>
+            <AutoGraphIcon fontSize='large' sx={{mr: 1}}/>
+            <Typography variant='h6' href='/' component='a' noWrap sx={{fontFamily: 'monospace', fontWeight: 700, color: 'inherit', textDecoration: 'none', letterSpacing: '.1rem'}}>
+              myCharts
+            </Typography>
+            <Box sx={{display: 'flex'}}>
+              <Switch checked={darkMode} onChange={handleChange} />
+              <Button onClick={handleVerify} color='inherit'>Verify Login</Button>
+              <Button onClick={handleLogout} color='inherit'>Logout</Button>
+              <Button href='/create' color='inherit'>Create</Button>
+            </Box>
+            <Box sx={{backgroundColor: alpha(theme.palette.common.white, 0.15), '&:hover': {backgroundColor: alpha(theme.palette.common.white, 0.25)}, p: 1, display: 'flex', justifyContent: 'center', alignItems: 'center', width: 'auto'}}>
+              <SearchIcon />
+              <InputBase placeholder='Search...' sx={{color: 'inherit', pl: 1, transition: theme.transitions.create('width'), width: 'auto'}} />
+            </Box>
+          </Toolbar>
+        </AppBar>
+        <Drawer variant='permanent' sx={{'& .MuiDrawer-paper': {bgcolor: 'primary.dark', color: 'white', py: 2, whiteSpace: 'nowrap', width: 240, transition: transition('enter'), boxSizing: 'border-box', ...(!open && {overflowX: 'hidden', transition: transition('leave'), width: theme.spacing(7)})}}}>
+          <Toolbar sx={{display: 'flex', alignItems: 'center', justifyContent: 'flex-end', px: [1]}} />
+          <Divider />
+          <List>
+            {[1, 2, 3].map(number => (
+              <ListItem key={number} disablePadding>
+                <ListItemButton>
+                  <ListItemIcon sx={{color: 'inherit'}}>
+                    <AutoGraphIcon />
+                  </ListItemIcon>
+                  <ListItemText primary={`Item ${number}`} sx={{color: 'inherit'}} />
+                </ListItemButton>
+              </ListItem>
+            ))}
+            <Divider />
+            {[1, 2, 3, 4, 5].map(number => (
+              <ListItem key={number} disablePadding>
+                <ListItemButton>
+                  <ListItemIcon sx={{color: 'inherit'}}>
+                    <AutoGraphIcon />
+                  </ListItemIcon>
+                  <ListItemText primary={`Chart ${number}`} sx={{color: 'inherit'}} />
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
+        </Drawer>
+        <Box component='main' sx={{flexGrow: 1, overflow: 'auto', pt: 15}}>
+          <Outlet />
+          <footer>
+            <Copyright />
+          </footer>
+        </Box>
+      </Box>
     </ThemeProvider>
   )
 }
