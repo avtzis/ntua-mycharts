7b4bc08e4c5a57d54774f5e20a0c7d20dbca475f
ntua-el19241 <el19241@mail.ntua.gr>
2023-04-12 15:51:58 +0300

    added responsive charts

--- ui/src/routes/Dashboard.js
+++ ui/src/routes/Dashboard.js
@@ -1,13 +1,17 @@
 import { Paper, Grid, Container, /* Box, Typography, */ Divider, ListItem, ListItemText, Stack } from '@mui/material'
 import React from 'react'
 import MyTable from '../components/MyTable'
-import image from '../temp/output-chart.png'
+//import image from '../temp/output-chart.png'
 import { useLoaderData } from 'react-router-dom'
+import highcharts from 'highcharts';
+import HighchartsReact from 'highcharts-react-official';
+import options from '../temp/chart-options1.json'
 
 const Dashboard = () => {
   const data = useLoaderData();
   const user = data.user;
   console.log(data);
+  console.log(options);
 
   return (
     <React.Fragment>
@@ -31,7 +35,8 @@ const Dashboard = () => {
           </Grid>
           <Grid item md={6}>
             <Paper variant='outlined' sx={{ width: '100%' }}>
-              <img src={image} alt='png' style={{ maxHeight: 450 }} />
+              {/* <img src={image} alt='png' style={{ maxHeight: 450 }} /> */}
+              <HighchartsReact highcharts={highcharts} options={{...options, chart:{...options.chart, height: 492}}} />
             </Paper>
           </Grid>
         </Grid>
