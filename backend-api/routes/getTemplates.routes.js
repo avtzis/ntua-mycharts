6de1b53bdebd16f2c0f43d6057332dbcfba7c7d2
ntua-el19241 <el19241@mail.ntua.gr>
2023-04-16 00:11:07 +0300

    removed non-functional code

--- backend-api/routes/getTemplates.routes.js
+++
@@ -1,7 +0,0 @@
-const templateModel  = require("../models/template.models");
-
-
-module.exports =  async (req, res) => {
-     const allTemplates = await templateModel.find({});
-     return res.json(allTemplates);
- };
