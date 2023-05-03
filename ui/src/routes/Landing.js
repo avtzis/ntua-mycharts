82b9867e52ddcc04af3b1274215582629ad6798d
Thanos <thanos.pyliotis@best-eu.org>
2023-05-03 12:41:13 +0300

    Changed structure of landing

--- ui/src/routes/Landing.js
+++ ui/src/routes/Landing.js
@@ -3,6 +3,7 @@ import { Paper, Container, Typography, Box } from '@mui/material'
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
 import ImageListItemBar from '@mui/material/ImageListItemBar';
+import Button from '@mui/material/Button';
 import image from '../temp/output-chart.png'
 import LoginButton from '../components/LoginButton'
 import { useOutletContext } from 'react-router-dom'
@@ -12,36 +13,10 @@ const Landing = () => {
 
   return (
     <React.Fragment>
-      <Container style={{
-        paddingTop: "10px",
-        width: "80%",
-        overflowX: "hidden",
-        overflowY: "auto",
-        whiteSpace: "nowrap"
-      }}>
-      <center> 
-      <ImageList sx={{ width: 1, height: 1/2 }}>
-      {itemData.map((item) => (
-       <ImageListItem key={item.img}>
-          <img
-            src={`${item.img}?w=248&fit=crop&auto=format`}
-            srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
-            alt={item.title}
-            loading="lazy"
-          />
-          <a href={item.URL}><ImageListItemBar
-            title={item.title}
-            position="below"
-          /></a> 
-        </ImageListItem>
-      ))}
-    </ImageList>
-    </center> 
-    </Container>
       {isLoggedIn || 
       <Container>
-        <Typography align='center'>
-          Press a diagram above to see how this works, or login with you Google account by pressing the button below
+        <Typography align='center' id="Top">
+          Press a diagram below to see how this works, or login with you Google account by pressing the button below
         </Typography>
         <Box sx={{display: 'flex', justifyContent: 'center'}}>
           <Box sx={{width: 200}}>
@@ -49,6 +24,29 @@ const Landing = () => {
           </Box>
         </Box>
       </Container>}
+
+      <center>
+      <ImageList sx={{ width: 0.75, height: 1/2 }}>
+        {itemData.map((item) => (
+        <ImageListItem key={item.img}>
+          <a href={item.URL}><ImageListItemBar
+            title={item.title}
+            position="below"
+          /></a> 
+          <img
+            src={`${item.img}?w=248&fit=crop&auto=format`}
+            srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+            alt={item.title}
+            loading="lazy"
+            style={{borderRadius: "20px"}}
+          />
+        </ImageListItem>
+        ))}
+      </ImageList>
+     </center>
+     <center>
+      <Button variant="outlined"><a href="#Top"> Go top </a></Button>
+     </center>
     </React.Fragment>
   )
 }
