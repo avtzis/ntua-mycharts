9c61717031293fa275c6f2c9b37316f6e6883721
ntua-el19230 <johnylo1996@gmail.com>
2023-05-10 11:54:13 +0300

    add template utility

--- server/utilities/createTemplates.js
+++ server/utilities/createTemplates.js
@@ -1,24 +1,54 @@
-const Templates=require('../models/Templates')
-const fs =require('fs')
-const path=require('path')
+const Templates = require("../models/Templates");
+const fs = require("fs");
+const path = require("path");
 
-const datapath=path.join(__dirname,'../data',"chart-template-options-1.csv")
-const csv1=fs.readFileSync(datapath)
+const types = [
+  "line",
+  "area",
+  "column",
+  "pie",
+  "dependencywheel",
+  "networkgraph",
+  "wordcloud",
+  "organization",
+  "polar",
+];
 
-const templates=[{
-    type:'line',
-    csv:csv1
-}]
 
-module.exports=async() =>{
-    templates.forEach(template =>{
-        Templates.findOrCreate(template,(err,data,created)=>{
-            if (err){
-                console.error(`There was an error finding or creating ${data.type} template`)
-            } else{
-                console.log(`${data.type} template has been ${created ? 'created sucessfully' : 'verified'}.`);
-            }
-        })
-    })
 
-}
+let templates = [];
+
+let import_csv = async (type, index) => {
+  return new Promise((resolve, reject) => {
+    datapath = path.join(
+      __dirname,
+      "../data",
+      `chart-template-options-${index + 1}.csv`
+    );
+
+    const csv = fs.readFileSync(datapath);
+    return resolve({ type: type, csv: csv });
+  });
+};
+
+module.exports = async () => {
+  Promise.all(types.map((type, index) => import_csv(type, index))).then(
+    (templates) => {
+      templates.forEach((template) => {
+        Templates.findOrCreate(template, (err, data, created) => {
+          if (err) {
+            console.error(
+              `There was an error finding or creating ${data.type} template`
+            );
+          } else {
+            console.log(
+              `${data.type} template has been ${
+                created ? "created sucessfully" : "verified"
+              }.`
+            );
+          }
+        });
+      });
+    }
+  );
+};
