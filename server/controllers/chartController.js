5867595d0fc97a4dde434f693f2fbc90fe6a590e
ntua-el19217 <el19217@mail.ntua.gr>
2023-05-10 19:59:58 +0300

    Implemented error catch

--- server/controllers/chartController.js
+++ server/controllers/chartController.js
@@ -8,12 +8,11 @@ exports.postValidate = async (req, res) => {
   const options = req.options;
 
   //TODO: validate
-  let error = undefined;
-  if(error) {
+  if(req.error) {
     return res.status(400).json({message: 'invalid options', valid: false});
   }
-
   return res.status(200).json({message: 'Chart valid', valid: true, options});
+
 }
 
 exports.postChart = async (req, res) => {
