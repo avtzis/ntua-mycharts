34637fcac87e0240d1daa8d15c0f7533cc4d1ba7
ntua-el19050 <el19050@mail.ntua.gr>
2023-05-10 10:59:52 +0300

    Added images for preview and deleted changes to count my commit
    We need preview images for the landing page + any changes I made in the landing page were committed by an account my VS Code created so I will recommit them

--- ui/src/routes/Landing.js
+++ ui/src/routes/Landing.js
@@ -25,79 +25,12 @@ const Landing = () => {
         </Box>
       </Container>}
 
-      <center>
-      <ImageList sx={{ width: 0.75, height: 1/2 }}>
-        {itemData.map((item) => (
-        <ImageListItem key={item.img}>
-          <a href={item.URL}><ImageListItemBar
-            title={item.title}
-            position="below"
-          /></a> 
-          <img
-            src={`${item.img}?w=248&fit=crop&auto=format`}
-            srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
-            alt={item.title}
-            loading="lazy"
-            style={{borderRadius: "20px"}}
-          />
-        </ImageListItem>
-        ))}
-      </ImageList>
-     </center>
-     <center>
-      <Button variant="outlined"><a href="./"> Go top </a></Button>
-     </center>
+      
     </React.Fragment>
   )
 }
    
 
-const itemData = [
-  {
-    img: image,
-    title: 'Line Diagram',
-    URL: './preview/line'
-  },
-  {
-    img: image,
-    title: 'Area Chart',
-    URL: './preview/area'
-  },
-  {
-    img: image,
-    title: 'Column Diagram',
-    URL: './preview/column'
-  },
-  {
-    img: image,
-    title: 'Pie Chart',
-    URL: './preview/pie'
-  },
-  {
-    img: image,
-    title: 'Dependenxy Wheel',
-    URL: './preview/dependencywheel'
-  },
-  {
-    img: image,
-    title: 'Network Graph',
-    URL: './preview/networkgraph'
-  },
-  {
-    img: image,
-    title: 'Word Cloud',
-    URL: './preview/wordcloud'
-  },
-  {
-    img: image,
-    title: 'Organisation Chart',
-    URL: './preview/organization'
-  },
-  {
-    img: image,
-    title: 'Polar Graph',
-    URL: './preview/polar'
-  },
-];
+
 
 export default Landing;
