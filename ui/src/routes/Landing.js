999541c60a487f2df4771b9076a9e8e86d063bb4
ntua-el19050 <el19050@mail.ntua.gr>
2023-05-10 11:23:31 +0300

    Changed names to images and finished landing page
    Landing page is finished, the previews of the images have been moved inside src for now.

--- ui/src/routes/Landing.js
+++ ui/src/routes/Landing.js
@@ -4,9 +4,18 @@ import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
 import ImageListItemBar from '@mui/material/ImageListItemBar';
 import Button from '@mui/material/Button';
-import image from '../temp/output-chart.png'
+import imageLine from '../previews/line.png';
+import imageArea from '../previews/area.png';
+import imageColumn from '../previews/column.png';
+import imagePie from '../previews/pie.png';
+import imageDependency from '../previews/dependency.png';
+import imageNetwork from '../previews/network.png';
+import imageWordcloud from '../previews/wordcloud.png';
+import imageOrg from '../previews/org.png';
+import imagePolar from '../previews/polar.png';
 import LoginButton from '../components/LoginButton'
 import { useOutletContext } from 'react-router-dom'
+import { lightBlue } from '@mui/material/colors';
 
 const Landing = () => {
   const { isLoggedIn } = useOutletContext();
@@ -25,12 +34,80 @@ const Landing = () => {
         </Box>
       </Container>}
 
+      <center>
+      <ImageList sx={{ width: 0.75, height: 1/2 }}>
+        {itemData.map((item) => (
+        <ImageListItem key={item.img}>
+          <a href={item.URL} style={{color: 'lightBlue'}}><ImageListItemBar
+            title={item.title}
+            position="below"
+          /></a> 
+          <img
+            src={`${item.img}?w=248&fit=crop&auto=format`}
+            srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+            alt={item.title}
+            loading="lazy"
+            style={{borderRadius: "20px"}}
+          />
+        </ImageListItem>
+        ))}
+      </ImageList>
+     </center>
+     <center>
+      <Button variant="outlined"><a href="./"> Go top </a></Button>
+     </center>
       
     </React.Fragment>
   )
 }
    
-
+const itemData = [
+  {
+    img: imageLine,
+    title: 'Line Diagram',
+    URL: './preview/line'
+  },
+  {
+    img: imageArea,
+    title: 'Area Chart',
+    URL: './preview/area'
+  },
+  {
+    img: imageColumn,
+    title: 'Column Diagram',
+    URL: './preview/column'
+  },
+  {
+    img: imagePie,
+    title: 'Pie Chart',
+    URL: './preview/pie'
+  },
+  {
+    img: imageDependency,
+    title: 'Dependency Wheel',
+    URL: './preview/dependencywheel'
+  },
+  {
+    img: imageNetwork,
+    title: 'Network Graph',
+    URL: './preview/networkgraph'
+  },
+  {
+    img: imageWordcloud,
+    title: 'Wordcloud',
+    URL: './preview/wordcloud'
+  },
+  {
+    img: imageOrg,
+    title: 'Organisation Chart',
+    URL: './preview/organization'
+  },
+  {
+    img: imagePolar,
+    title: 'Polar Graph',
+    URL: './preview/polar'
+  },
+];
 
 
 export default Landing;
