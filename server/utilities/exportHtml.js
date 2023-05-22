25a05bcb59a8fe55705354262bb0649f0603d724
avtzis <avtzialtan@hotmail.com>
2023-05-22 17:09:21 +0300

    improved html export to be functional for all chart types

--- server/utilities/exportHtml.js
+++ server/utilities/exportHtml.js
@@ -3,6 +3,14 @@ module.exports = options => {
   <script src="https://code.highcharts.com/modules/exporting.js"></script>
   <script src="https://code.highcharts.com/modules/export-data.js"></script>
   <script src="https://code.highcharts.com/modules/accessibility.js"></script>
+  <script src="https://code.highcharts.com/modules/series-label.js"></script>
+  <script src="https://code.highcharts.com/modules/annotations.js"></script>
+  <script src="https://code.highcharts.com/modules/sankey.js"></script>
+  <script src="https://code.highcharts.com/modules/dependency-wheel.js"></script>
+  <script src="https://code.highcharts.com/modules/networkgraph.js"></script>
+  <script src="https://code.highcharts.com/modules/wordcloud.js"></script>
+  <script src="https://code.highcharts.com/modules/organization.js"></script>
+  <script src="https://code.highcharts.com/highcharts-more.js"></script>
   
   <figure class="highcharts-figure">
       <div id="container"></div>
